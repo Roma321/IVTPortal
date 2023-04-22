5427fa9cece2c561442165dd4f46335b6ba2fd70
IlyaS0k <ilyasokolov63@mail.ru>
2023-04-22 14:35:11 +0300

    таблица пересдач + неполностью добавление пересдачи

--- src/main/java/app/controllers/ResitController.java
+++ src/main/java/app/controllers/ResitController.java
@@ -1,43 +1,48 @@
 package app.controllers;
 
 import app.models.Resit;
+import app.models.Teacher;
 import app.services.resit.ResitService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Date;
+import java.util.Map;
 
 @Controller
 public class ResitController {
     private final ResitService resitService;
 
+    @Autowired
     public ResitController(ResitService resitService) {
         this.resitService = resitService;
     }
 
     @PostMapping("/resit/add")
-    public String addResit(Model model, @RequestBody Resit resit) {
-        resitService.addResit(resit);
+    public String addResit(@RequestParam Map<String, Object> map) {
 
-        model.addAttribute("date", resit.getDate().toLocalDate());
-        model.addAttribute("time", resit.getTimeStart().toLocalTime());
-        model.addAttribute("isOnline", resit.getOnline() ? "Онлайн" : "В корпусе");
-
-        return "resit/resit";
+        return "redirect:/resit/add";
     }
-
-    @DeleteMapping("/resit/delete/{id}")
-    public String deleteResit(Model model, @PathVariable Integer id) {
+    @GetMapping("/resit/add")
+    public String addResit() {
+        //Нужен сервис для teachers, auditors, subjects, чтобы их в combobox показывать
+        //Iterable<Teacher> teachers = teacherService.getAll()
+        //model.addAttribute("teachers", teachers);
+        //....
+        return "resit/add";
+    }
+    @PostMapping("/resit/delete/{id}")
+    public String deleteResit(@PathVariable Integer id) {
         resitService.deleteResit(id);
-
-        model.addAttribute("date", "Удалено");
-        model.addAttribute("time", "Удалено");
-        model.addAttribute("isOnline", "Удалено");
-
-        return "resit/resit";
+        return "redirect:/resit/all";
     }
 
     @GetMapping("/resit/all")
-    public Iterable<Resit> getAll(Model model) {
-        return resitService.getAll();
+    public String getAll(Model model) {
+        Iterable<Resit> resits = resitService.getAll();
+        model.addAttribute("resits", resits);
+        return "resit/resit";
     }
 }
