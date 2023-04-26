6dcf1464bfbee39d53cfa44480bebaffa13947a2
roman <mamaew.roma2000@yandex.ru>
2023-04-26 20:50:18 +0300

    Улучшил UX заполнения расписания – теперь показываются только доступные номера пар, преподаватели и аудитории.

--- src/main/java/app/repositories/LessonScheduleRepository.java
+++ src/main/java/app/repositories/LessonScheduleRepository.java
@@ -4,11 +4,17 @@ import app.models.LessonSchedule;
 import app.models.Teacher;
 import app.models.enums.LessonType;
 import app.models.enums.WeekDay;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
 
 public interface LessonScheduleRepository extends CrudRepository<LessonSchedule, Integer> {
 
     LessonSchedule findByTeacherAndWeekDayAndLessonNumberAndLessonType(
             Teacher teacher, WeekDay weekDay, int lessonNumber, LessonType lessonType
     );
+
+
+    List<LessonSchedule> findLessonSchedulesByGroupsGroupId(int group);
 }
